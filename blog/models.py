b67be5c68be2f7f62c80a06915d796a9514383ed
Vikrant Reddy <vikrantreddy@hotmail.com>
2022-05-29 12:10:51 +0530

    Added blog base

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField()
+    author = models.CharField(max_length=64)
+    topic = models.CharField(max_length=64)
+    intro = models.TextField()
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_added']
