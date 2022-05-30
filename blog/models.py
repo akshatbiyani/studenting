c69e4c09ca1e9abcd3f711646bf077100309d7cd
Vikrant Reddy <vikrantreddy@hotmail.com>
2022-05-30 10:51:46 +0530

    Templating init

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
     image = models.ImageField()
     author = models.CharField(max_length=64)
     topic = models.CharField(max_length=64)
-    intro = models.TextField()
+    intro = models.Te0xtField()
     body = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,14 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
+
+
+def blog_post(request):
+    slug = request.url.split('/')[-1]
+    return render(request,'blog/blog_post.html',{
+        "post":Post.object.filter(slug=slug)
+    })
+
+
+def topic_page(request):
+    posts = Post.objects.filter(topic="test")
