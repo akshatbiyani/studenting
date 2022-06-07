c37dced7a668a46d440adc22f5ba9368d6b7b49f
Vikrant Reddy <vikrantreddy@hotmail.com>
2022-06-08 02:05:09 +0530

    Database config

--- Studenting/settings.py
+++ Studenting/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 
+
 import os
 from pathlib import Path
 import django_heroku
@@ -85,13 +86,17 @@ WSGI_APPLICATION = 'Studenting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
